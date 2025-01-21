089b8f88bbe973d0c2dbeb26b8edd2cdbd4dcede
UZB02 <muhsinbekmirzamatov3@gmail.com>
2025-01-21 17:39:44 +0500

    catalog post metod

--- index.js
+++ index.js
@@ -251,6 +251,10 @@ app.get('/api/books',(req, res)=>{
     res.send(books)
 })
 
+app.get('/api/catalogs',(req, res)=>{
+  res.send(catalogs)
+})
+
 app.get('/api/catologs/:id',(req, res)=>{
   const {id} = req.params
   const catalog = catalogs.filter((c) => c.categori_id === parseInt(id));
@@ -290,3 +294,20 @@ app.post('/api/admin/login', cors(),(req, res)=>{
         res.status(401).json({message: 'Foydalanuvchi topilmadi'})
     }
 }); //
+
+app.post('/api/addcatalog',(req, res)=>{
+  const {title, categori_id} = req.body;
+  const newCatalog = {
+    id: catalogs.length + 1,
+    title,
+    categori_id,
+    books: books.filter((item) => item.catalog_id === 5).length,
+    audiobooks: books.filter(
+      (item) => item.catalog_id === 5 && item.tipe === "audio"
+    ).length,
+    book: books.filter((item) => item.catalog_id === 5 && item.tipe === "file")
+      .length,
+  };
+  catalogs.push(newCatalog);
+  res.send(newCatalog)
+})
